3f3a94677ffa13226e40cc11c1dfb1940c7bd2c4
sudhanshu shekhar jha <sudhanshushekharjha330@gmail.com>
2026-09-17 22:44:07 +0530

    controllers modified

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts,createProducts } from "../controllers/productsControllers.js";
+import { getProducts,createProducts,getProduct,updateProduct,deleteProduct } from "../controllers/productsControllers.js";
 
 const router=express.Router();
 
-router.get("/" , getAllProducts);
+router.get("/" , getProducts);
+router.get("/:id" , getProduct);
 router.post("/" , createProducts);
+router.put("/:id" , updateProduct);
+router.delete("/:id" , deleteProduct);
 
 export default router;

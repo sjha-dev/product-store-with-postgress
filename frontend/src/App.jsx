c6ec9aecaa1ca75675c867d4968c81530da439ab
sudhanshu shekhar jha <sudhanshushekharjha330@gmail.com>
2026-09-23 23:47:46 +0530

    Navbar component setUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
